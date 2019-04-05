3af1c545086811e4dd812a7e4a7176c89454efc6
yamandabbagh <yd50ty@gmail.com>
2019-04-05 11:09:33 -0700

    dshard with epoch

--- iotex-core/cli/ioctl/cmd/dshard/dnodedelegate.go
+++ iotex-core/cli/ioctl/cmd/dshard/dnodedelegate.go
@@ -9,13 +9,13 @@ package dshard
 import (
 	"context"
 	"fmt"
-	"math/big"
-	"strconv"
-	"strings"
+	//"math/big"
+	//"strconv"
+	//"strings"
 
 	"github.com/spf13/cobra"
 
-	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/alias"
+	//"github.com/iotexproject/iotex-core/cli/ioctl/cmd/alias"
 	"github.com/iotexproject/iotex-core/cli/ioctl/cmd/bc"
 	"github.com/iotexproject/iotex-core/cli/ioctl/util"
 	"github.com/iotexproject/iotex-core/protogen/iotexapi"
@@ -32,7 +32,7 @@ var dnodeDelegateCmd = &cobra.Command{
 }
 
 func ddelegate() string {
-	status := map[bool]string{true: "active", false: ""}
+	//status := map[bool]string{true: "active", false: ""}
 	if epochNum == 0 {
 		chainMeta, err := bc.GetChainMeta()
 		if err != nil {
@@ -40,51 +40,51 @@ func ddelegate() string {
 		}
 		epochNum = chainMeta.Epoch.Num
 	}
-	/* 
-	conn, err := util.ConnectToEndpoint()
-	if err != nil {
-		return err.Error()
-	}
-	defer conn.Close()
-	cli := iotexapi.NewAPIServiceClient(conn)
-	request := &iotexapi.GetEpochMetaRequest{EpochNumber: epochNum}
-	ctx := context.Background()
-	response, err := cli.GetEpochMeta(ctx, request)
-	if err != nil {
-		return err.Error()
-	}
+	
+		conn, err := util.ConnectToEndpoint()
+		if err != nil {
+			return err.Error()
+		}
+		defer conn.Close()
+		cli := iotexapi.NewAPIServiceClient(conn)
+		request := &iotexapi.GetEpochMetaRequest{EpochNumber: epochNum}
+		ctx := context.Background()
+		response, err := cli.GetEpochMeta(ctx, request)
+		if err != nil {
+			return err.Error()
+		}
 
-	epockData := response.EpochData
-	aliases := alias.GetAliasMap()
-	formataliasLen := 0
-	for _, delegateInfo := range response.BlockProducersInfo {
-		if len(aliases[delegateInfo.Address]) > formataliasLen {
-			formataliasLen = len(aliases[delegateInfo.Address])
+		epockData := response.EpochData
+		/*aliases := alias.GetAliasMap()
+		formataliasLen := 0
+		for _, delegateInfo := range response.BlockProducersInfo {
+			if len(aliases[delegateInfo.Address]) > formataliasLen {
+				formataliasLen = len(aliases[delegateInfo.Address])
+			}
 		}
-	}
-	lines := make([]string, 0)
-	lines = append(lines, fmt.Sprintf("Epoch: %d,  Start block height: %d,"+
-		"  Total blocks in epoch: %d\n", epockData.Num, epockData.Height, response.TotalBlocks))
-	formatTitleString := "%-41s   %-5s   %-" + strconv.Itoa(formataliasLen) +
-		"s   %-6s   %-6s   %s"
-	formatDataString := "%-41s   %5d   %-" + strconv.Itoa(formataliasLen) +
-		"s   %-6s   %-6s   %s"
-	lines = append(lines, fmt.Sprintf(formatTitleString,
-		"Address", "Index", "Alias", "Status", "Blocks", "Votes"))
-	for index, bp := range response.BlockProducersInfo {
-		votes, ok := big.NewInt(0).SetString(bp.Votes, 10)
-		if !ok {
-			return "failed to convert votes into big int"
-		}
-		production := ""
-		if bp.Active {
-			production = strconv.Itoa(int(bp.Production))
-		}
-		lines = append(lines, fmt.Sprintf(formatDataString, bp.Address, index+1,
-			aliases[bp.Address], status[bp.Active], production,
-			util.RauToString(votes, util.IotxDecimalNum)))
-	}*/
+		lines := make([]string, 0)
+		lines = append(lines, fmt.Sprintf("Epoch: %d,  Start block height: %d,"+
+			"  Total blocks in epoch: %d\n", epockData.Num, epockData.Height, response.TotalBlocks))
+		formatTitleString := "%-41s   %-5s   %-" + strconv.Itoa(formataliasLen) +
+			"s   %-6s   %-6s   %s"
+		formatDataString := "%-41s   %5d   %-" + strconv.Itoa(formataliasLen) +
+			"s   %-6s   %-6s   %s"
+		lines = append(lines, fmt.Sprintf(formatTitleString,
+			"Address", "Index", "Alias", "Status", "Blocks", "Votes"))
+		for index, bp := range response.BlockProducersInfo {
+			votes, ok := big.NewInt(0).SetString(bp.Votes, 10)
+			if !ok {
+				return "failed to convert votes into big int"
+			}
+			production := ""
+			if bp.Active {
+				production = strconv.Itoa(int(bp.Production))
+			}
+			lines = append(lines, fmt.Sprintf(formatDataString, bp.Address, index+1,
+				aliases[bp.Address], status[bp.Active], production,
+				util.RauToString(votes, util.IotxDecimalNum)))
+		}*/
 	//return strings.Join(lines, "\n")
-	return fmt.Sprintf("epocho; %d", epochNum)
+	return fmt.Sprintf("epocho; %d %d", epochNum,epockData.Num)
 
 }
